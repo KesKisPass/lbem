bca349f159a9b8302214d1a24e5afbac4390ef29
JG <jg.buret@gmail.com>
2015-01-14 16:09:47 +0100

    Routes returns saved in @variable to be able to use them from a page.haml

--- app/controllers/login.rb
+++ app/controllers/login.rb
@@ -16,7 +16,8 @@ Lbem::App.controllers :login do
 	# @return [String] a json string representing a hash
 	# @note the hash provide for key 'fields' a hash containing an array of fields' names the user must provide and their key
 	get :form, map: 'login/create/form' do
-		{ fields: User.required_parameters(:create) }.to_json
+		@create_form = { fields: User.required_parameters(:create) }
+		@create_form.to_json
 	end
 
 	# creates a user
@@ -26,11 +27,12 @@ Lbem::App.controllers :login do
 	# @param password_confirmation [String]
 	# @return [String] a json string reprensenting a map with keys 'success' (boolean) and 'text' (string)
 	post :create do
-		begin
+		@create_from_form = begin
 			User.create_from_form(params)
 		rescue ArgumentError => e
 			{ success: false, text: e.message }
-		end.to_json
+		end
+		@create_from_form.to_json
 	end
 
 	## Authentification
@@ -42,7 +44,8 @@ Lbem::App.controllers :login do
 	# @note it returns a json string representing a map.
 	# @note the map provide for key 'fields' an array of fields' names the user must provide
 	get :form, map: 'login/check/form' do
-		{ fields: User.required_parameters(:check) }.to_json
+		@check_form = { fields: User.required_parameters(:check) }
+		@check_form.to_json
 	end
 
 	# log a user if authentification succeed
@@ -53,7 +56,8 @@ Lbem::App.controllers :login do
 	post :check do
 		user = User.authenticate(params[:email], params[:password])
 		set_current_user(user)
-		{ success: !!user }.to_json
+		@check = { success: !!user }
+		@check.to_json
 	end
 
 	## Log out
--- models/user.rb
+++ models/user.rb
@@ -1,92 +1,92 @@
 class User
 
-	include Mongoid::Document
+  include Mongoid::Document
 
-	field :email,		type: String
-	field :password,	type: String
-	field :nickname,	type: String
+  field :email,        type: String
+  field :password,  type: String
+  field :nickname,  type: String
 
-	validates_presence_of		:email
-	validates_uniqueness_of	:email,    	case_sensitive: false
-	validates_format_of		:email,    	with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates_presence_of    :email
+  validates_uniqueness_of :email,     case_sensitive: false
+  validates_format_of        :email,     with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
-	validates_presence_of		:nickname
-	validates_uniqueness_of	:nickname,	case_sensitive: true
-	validates_format_of		:nickname,	with: /[A-Za-z]{1,10}/
+  validates_presence_of    :nickname
+  validates_uniqueness_of :nickname,  case_sensitive: true
+  validates_format_of        :nickname,  with: /[A-Za-z]{1,10}/
 
-	def to_s
-		"#{nickname} '#{email}'"
-	end
+  def to_s
+    "#{nickname} '#{email}'"
+  end
 
 # Authentification
 
-	# @return: user or nil
-	def self.authenticate(email, pwd)
-		return unless email.present? && pwd.present?
-		user = where(:email => /#{Regexp.escape(email)}/i).first
-		user && user.correct_password?(pwd) ? user : nil
-	end
+  # @return: user or nil
+  def self.authenticate(email, pwd)
+    return unless email.present? && pwd.present?
+    user = where(:email => /#{Regexp.escape(email)}/i).first
+    user && user.correct_password?(pwd) ? user : nil
+  end
 
-	# @return: true or false
-	def correct_password?(to_check)
-		::BCrypt::Password.new(password) == to_check
-	end
+  # @return: true or false
+  def correct_password?(to_check)
+    ::BCrypt::Password.new(password) == to_check
+  end
 
-	def self.format_of( fmt )
-		{
-			email: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i ,
-			nickname: /[A-Za-z]{1,10}/ ,
-		}[ fmt ]
-	end
+  def self.format_of( fmt )
+    {
+      email: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i ,
+      nickname: /[A-Za-z]{1,10}/ ,
+    }[ fmt ]
+  end
 
 # Creation
 
-	# get the required parameters for User creation
-	#
-	# @param action [Symbol] the action to perform
-	# @note available actions are :create, password_modif
-	# @return [Array] names of parameters to provide
-	def self.required_parameters(action)
-		{
-			create: {
-				keys: [ 'email', 'nickname', 'password', 'password_confirmation' ],
-				names: [ 'Email', 'Nickname', 'Password', 'Password confirmation' ]
-			},
-			check: {
-				keys: [ 'email', 'password' ],
-				names: [ 'Email', 'Password' ]
-			},
-			password_modif: {
-				keys: [ 'actual_password', 'new_password', 'new_password_confirmation' ],
-				names: [ 'Actual assword', 'New password', 'New password confirmation' ]
-			}
-		}[ action ]
-	end
+  # get the required parameters for User creation
+  #
+  # @param action [Symbol] the action to perform
+  # @note available actions are :create, password_modif
+  # @return [Array] names of parameters to provide
+  def self.required_parameters(action)
+    {
+      create: {
+        keys: [ 'email', 'nickname', 'password', 'password_confirmation' ],
+        names: [ 'Email', 'Nickname', 'Password', 'Password confirmation' ]
+      },
+      check: {
+        keys: [ 'email', 'password' ],
+        names: [ 'Email', 'Password' ]
+      },
+      password_modif: {
+        keys: [ 'actual_password', 'new_password', 'new_password_confirmation' ],
+        names: [ 'Actual assword', 'New password', 'New password confirmation' ]
+      }
+    }[ action ]
+  end
 
-	def self.encrypt_password(to_save)
-		::BCrypt::Password.create(to_save).to_s
-	end
+  def self.encrypt_password(to_save)
+    ::BCrypt::Password.create(to_save).to_s
+  end
 
-	# create a user from params. Also check uniqueness first
-	#
-	# @param params [Hash] parameters provided by the user
-	# @return [Hash]
-	# @note called from controler login
-	def self.create_from_form(params)
-		[
-			{ msg: 'Missing parameters',				rule: lambda { |p| ( required_parameters(:create)[:keys] - p.keys ).empty? } },
-			{ msg: 'Password confirmation failed',	rule: lambda { |p| p['password'] == p['password_confirmation'] } },
-			{ msg: 'Email don\'t respect format',		rule: lambda { |p| p['email'] =~ User.format_of(:email) } },
-			{ msg: 'Nickname don\'t respect format', rule: lambda { |p| p['nickname'] =~ User.format_of(:nickname) } },
-			{ msg: 'Email already taken',				rule: lambda { |p| User.where(email: p['email']).empty? } },
-			{ msg: 'Nickname already taken',			rule: lambda { |p| User.where(nickname: p['nickname']).empty? } }
-		].each { |r| raise ArgumentError, r[:msg] unless r[:rule].call(params) }
+  # create a user from params. Also check uniqueness first
+  #
+  # @param params [Hash] parameters provided by the user
+  # @return [Hash]
+  # @note called from controler login
+  def self.create_from_form(params)
+    [
+      { msg: 'Missing parameters',                    rule: lambda { |p| ( required_parameters(:create)[:keys] - p.keys ).empty? } },
+      { msg: 'Password confirmation failed',     rule: lambda { |p| p['password'] == p['password_confirmation'] } },
+      { msg: 'Email don\'t respect format',        rule: lambda { |p| p['email'] =~ User.format_of(:email) } },
+      { msg: 'Nickname don\'t respect format', rule: lambda { |p| p['nickname'] =~ User.format_of(:nickname) } },
+      { msg: 'Email already taken',                    rule: lambda { |p| User.where(email: p['email']).empty? } },
+      { msg: 'Nickname already taken',             rule: lambda { |p| User.where(nickname: p['nickname']).empty? } }
+    ].each { |r| raise ArgumentError, r[:msg] unless r[:rule].call(params) }
 
-		attributes = {}
-		params['password'] = encrypt_password(params['password'])
-		( required_parameters(:create)[:keys] - ['password_confirmation'] ).each { |p| attributes[p] = params[p] }
-		raise ArgumentError, 'Internal server error' unless User.new(attributes).save
-		{ success: true, text: "Registration succeed" }
-	end
+    attributes = {}
+    params['password'] = encrypt_password(params['password'])
+    ( required_parameters(:create)[:keys] - ['password_confirmation'] ).each { |p| attributes[p] = params[p] }
+    raise ArgumentError, 'Internal server error' unless User.new(attributes).save
+    { success: true, text: "Registration succeed" }
+  end
 
 end
