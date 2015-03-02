066a7167eb76723b480b665b61e515238c5285bf
JG <jg.buret@gmail.com>
2015-03-02 15:19:20 +0100

    Added exceptions being raised in ContactList model. ContactLists controller now handle previous errors

--- models/contact_list.rb
+++ models/contact_list.rb
@@ -16,33 +16,48 @@ class ContactList
   ## invite contact
   #
   # @param user_to_invite [User] user to invite by current user
+  # @raise [ArgumentError] if user_to_invite is already in pending, or in contacts
+  # @return [TrueClass] if invited. Nil otherwhise
   def invite_contact(user_to_invite)
     return if user._id == user_to_invite._id
-    alreadyInvited = user.pending_contacts.where(requestee_id: user_to_invite._id).length.zero?
-    alreadyInContactList = contacts.where(_id: user_to_invite._id).length.zero?
-    user.pending_contacts.create!( requestee: user_to_invite ) if alreadyInvited and alreadyInContactList
+    raise ArgumentError, 'Already a contact' if contacts.where(_id: user_to_invite._id).exists?
+    raise ArgumentError, 'You already asked this user' if user.pending_contacts.where(requestee_id: user_to_invite._id).exists?
+    raise ArgumentError, 'This user already asked you' if user.pending_contacts.where(requester_id: user_to_invite._id).exists?
+    user.pending_contacts.create!( requestee: user_to_invite )
+    true
   end
 
   ## delete contact in current and old user
   #
   # @param user_to_delete [User] user to remove in current user contact list
+  # @raise [ArgumentError] if user_to_remove is not in contact_list
+  # @return [TrueClass] if deleted
   def remove_contact(user_to_remove)
+    raise ArgumentError, 'Not a contact' unless contacts.where(_id: user_to_invite._id).exists?
     contacts.delete(user_to_remove)
     user_to_remove.contact_list.contacts.delete(user)
+    true
   end
 
   ## accept invitation
   #
   # @param user_to_accept [User] requester to accept
+  # @raise [ArgumentError] if not requested
+  # return [TrueClass] if accepted
   def accept_invitation(user_to_accept)
-    add_contact(user_to_accept) unless PendingContact.where(requester_id: user_to_accept._id, requestee_id: user._id).delete.zero? 
+    raise ArgumentError, 'Contact request not found' if PendingContact.where(requester_id: user_to_accept._id, requestee_id: user._id).delete.zero? 
+    add_contact(user_to_accept)
+    true
   end
 
   ## cancel invitation
   #
   # @param user_to_cancel [User] requester invitation to cancel by current user
+  # @raise [ArgumentError] if not requested
+  # @return [TrueClass] if canceled
   def cancel_invitation(user_to_cancel)
-    PendingContact.or({requester_id: user_to_cancel._id, requestee_id: user._id}, {requester_id: user._id, requestee_id: user_to_cancel._id}).delete
+    raise ArgumentError, 'Contact request not found' if PendingContact.or({requester_id: user_to_cancel._id, requestee_id: user._id}, {requester_id: user._id, requestee_id: user_to_cancel._id}).delete.zero?
+    true
   end
 
 private
